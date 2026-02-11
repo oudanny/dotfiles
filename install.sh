6396095f95d53bf896372814e6db8c3a6ef2e9c8
Daniel McAllister-Ou <135235559+oudanny@users.noreply.github.com>
2026-02-11 11:10:58 -0600

    Fix install.sh
    Check for existence before backing up and remove old files.

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 script_dir=$(dirname "$(readlink -f "$0")")
 
-mv "$HOME/.bash_profile" "$HOME/.bash_profile_backup"
-mv "$HOME/.bashrc" "$HOME/.bashrc_backup"
 
+[ -f "$HOME/.bash_profile" ] && mv "$HOME/.bash_profile" "$HOME/.bash_profile_backup"
+[ -f "$HOME/.bashrc" ] && mv "$HOME/.bashrc" "$HOME/.bashrc_backup"
 
+rm -f $HOME/.bash_profile
+rm -f $HOME/.bashrc
 # Create symlinks
 ln -s "$script_dir/bash_profile" "$HOME/.bash_profile"
 ln -s "$script_dir/bashrc" "$HOME/.bashrc"
